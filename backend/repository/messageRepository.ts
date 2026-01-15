75be7760c9ab4bd87b1d43252d4b6154d0c7b1e9
EbingerPhilip <philip.ebinger@gmx.at>
2026-01-15 15:34:07 +0100

    encryp messages and scheduled messages

--- backend/repository/messageRepository.ts
+++ backend/repository/messageRepository.ts
@@ -1,22 +1,35 @@
 //import { pool } from "../config/db"; // ES Module import - does not work with current export style
 const pool = require("../config/db"); // CommonJS require - works with current export style
+import { json } from "stream/consumers";
+import { encrypt,decrypt } from "../config/encryption";
+
+interface MessageRow {
+  messageId: number;
+  sender: number;
+  nicknames: string;
+  text: string | null;
+  timeSent: Date;
+}
+
 
 class MessageRepository {
   async createMessage(userId: number, chatId: number, text: string): Promise<number> {
     const sql = `INSERT INTO messages (sender, chatid, text, timeSent) VALUES (?, ?, ?, NOW())`;
-    const [result]: any = await pool.execute(sql, [userId, chatId, text]);
+    const [result]: any = await pool.execute(sql, [userId, chatId, encrypt(text)]);
     return result.insertId; 
   }
 
   async getMessageById(messageId: number): Promise<any> {
     const sql = `SELECT * FROM messages WHERE messageId = ?`;
     const [rows]: any = await pool.execute(sql, [messageId]);
-    return rows[0];
+    const message = rows[0];
+    message.text = decrypt(message.text)
+    return message;
   }
 
   async updateMessage(messageId: number, newText: string): Promise<void> {
     const sql = `UPDATE messages SET text = ? WHERE messageId = ?`;
-    await pool.execute(sql, [newText, messageId]);
+    await pool.execute(sql, [encrypt(newText), messageId]);
   }
 
   async deleteMessage(messageId: number): Promise<void> {
@@ -38,7 +51,11 @@ class MessageRepository {
     ORDER BY m.timeSent ASC
   `;
   const [rows]: any = await pool.execute(sql, [chatId]);
-  return rows;
+  return rows.map((row: MessageRow) => ({
+    ...row,
+    text: row.text ? decrypt(row.text) : null
+  }));
+
 }
 
 // For decoupling instant buddy chats: replace real userId with fake userId (e.g., 1)
@@ -49,10 +66,12 @@ class MessageRepository {
 
 
 // Cleanup to delete all messages when a chat was deleted
+//this is done automatically by mysql
 async deleteMessagesByChat(chatId: number): Promise<void> {
 const sql = `DELETE FROM messages WHERE chatid = ?`;
 await pool.execute(sql, [chatId]);
 }
+
 
 }
 
